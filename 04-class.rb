4716dd4c65a315671bac54316a9a94281eba445d
davek@hendrix <dave@thekilempire.com>
2013-12-15 13:03:07 -0500

    refactored calc

--- 04-class.rb
+++ 04-class.rb
@@ -19,18 +19,12 @@ class Fizzbuzz
     JSON.generate(hash(@max))
   end
 
-
   private
   def calc(n)
-    if (n % 15 == 0)
-      "FizzBuzz"
-    elsif (n % 3 == 0)
-      "Fizz"
-    elsif (n % 5 == 0)
-      "Buzz"
-    else
-      n
-    end
+    "FizzBuzz" if n % 15 == 0
+    "Fizz" if n % 3 == 0
+    "Buzz" if n % 5 == 0
+    n
   end
 
   def hash(n)
